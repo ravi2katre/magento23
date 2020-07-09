2f3cc824fab98929f39f66a0c99a2b65a76a89c1
ravi <rkatre@tiuconsulting.com>
2020-07-09 15:59:52 +0000

    magento install

--- app/code/Gugliotti/News/registration.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Module registration file
- */
-\Magento\Framework\Component\ComponentRegistrar::register(
-    \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Gugliotti_News',
-    __DIR__
-);
